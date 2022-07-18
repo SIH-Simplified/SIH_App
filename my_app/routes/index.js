8dff6f6200d4b2ede7ae13502aa6de7014deaf9b
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-18 18:59:54 +0530

    Added a middleware to set if the cookies are set or not

--- my_app/routes/index.js
+++ my_app/routes/index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.all("/", (req, res, next) => {
+  try {
+    const cookies = req.cookies;
+    if (cookies === null || cookies === undefined || Object.keys(cookies).length === 0)
+      res.redirect("/login");
+    console.log("cookies = ", cookies);
+  } catch (error) {
+    next(error);
+  }
+  next();
+})
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
