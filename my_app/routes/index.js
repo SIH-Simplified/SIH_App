9b09953de52a1e270375f838a27d71855cb79365
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-18 18:38:57 +0530

    Added jsonwebtokens as developer dependency and add /login route

--- my_app/routes/index.js
+++ my_app/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+})
 module.exports = router;
