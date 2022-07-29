8a558365a03650dc75e563517c9fa5d39ccd8651
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-29 21:22:44 +0530

    Added basic setup for cloudinary api for uploading images and files

--- my_app/cloudinary/index.js
+++ my_app/cloudinary/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: "student",
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET,
     secure: true
--- my_app/routes/users.js
+++ my_app/routes/users.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+const cloudinary = require("../cloudinary/index");
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/resume-1", (req, res) => {
-  res.render("resume-1");
+router.get("/resume-1", async (req, res, next) => {
+  try {
+    await cloudinary.uploader.upload("f:/SIH/SIH_App/my_app/public/simplifiedsihfinal20220422114630.pdf",
+      { public_id: Math.random() * 10 * 201 }, function (error, result) {
+        if (error) {
+          next(error);
+        }
+        console.log(result);
+      })
+    console.log(cloudinary.config());
+    res.render("resume-1");
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.get("/resume-2", (req, res) => {
