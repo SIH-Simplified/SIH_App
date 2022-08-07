cc3f407a1db0f55bed0a75f8e0563e2641c76cb2
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-07 15:24:02 +0530

    Added a admin schema from the database

---
+++ my_app/models/admin.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+    adminName: {
+        type: String,
+        required: [true, "Please provide a admin username"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"]
+    },
+    isAdmin: {
+        type: Number,
+        min: [0, "Privilage cannot be lower than level 0"],
+        max: [2, "Privilage level cannot be greater than level 2"],
+        required: true
+    }
+})
+
+// To know more about isAdmin feild look in the admin.md file in the root directory
+
+const Admin = mongoose.model("Admin", adminSchema);
+module.exports = Admin;
