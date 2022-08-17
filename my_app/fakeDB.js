3a7b07188b19a119ac0aaa39b15abebb4c9a01e1
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-17 10:17:50 +0530

    Added new version of admin and superadmin dashboard

---
+++ my_app/fakeDB.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    }, {
+        name: "Akane",
+        age: 21,
+        gender: "Female"
+    },
+]
