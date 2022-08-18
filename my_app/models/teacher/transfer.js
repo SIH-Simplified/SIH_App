735c359cdda1d22c230662027e39f0b2b80bacd3
lakshyarawat1 <lakshya.paramount@gmail.com>
2022-08-18 21:50:25 +0530

    added transfer route and db
    transfer route : validator error coming
    route
    
    bin : showing the error ,  port 3000 is already in use

---
+++ my_app/models/teacher/transfer.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const transferDB = require("../../transferDB");
+
+const transferSchema = new mongoose.Schema({
+  school_type: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  mobile_number: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  present_school: {
+    type: String,
+    required: true,
+  },
+  present_district: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  current_designation: {
+    type: String,
+    required: true,
+  },
+  date_of_joining: {
+    type: String,
+    required: true,
+  },
+  marital_status: {
+    type: String,
+    required: true,
+  },
+});
+
+const transfer = mongoose.model("Transfer", transferSchema);
+module.exports = transfer;
--- my_app/routes/teacher.js
+++ my_app/routes/teacher.js
@@ -1,196 +1,284 @@
-require('dotenv').config()
-const teacher = require('../models/teacher')
-const JWT = require('jsonwebtoken')
-const express = require('express')
-const bcrypt = require('bcryptjs')
-const checkAdmin = require('../middlewares/checkAdmin')
-const { check, validationResult, checkSchema } = require('express-validator')
-const cloudinary = require('../cloudinary/index');
+require("dotenv").config();
+const teacher = require("../models/teacher");
+const mongoose = require('mongoose')
+const JWT = require("jsonwebtoken");
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const Transfer = require("../models/teacher/transfer");
+const transferDB = require('../transferDB')
+const checkAdmin = require("../middlewares/checkAdmin");
+const { check, validationResult, checkSchema } = require("express-validator");
+const cloudinary = require("../cloudinary/index");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('teacher/index');
-})
-router.get('/login', (req, res) => {
-    res.render('teacher/login')
-})
-router.get('/register', (req, res) => {
-    res.render('teacher/register')
-})
+router.get("/", (req, res) => {
+  res.render("teacher/index");
+});
+router.get("/login", (req, res) => {
+  res.render("teacher/login");
+});
+router.get("/register", (req, res) => {
+  res.render("teacher/register");
+});
 
-router.post('/login', async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-        const Teacher = await teacher.find({ email });
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const Teacher = await teacher.find({ email });
 
-        if (!Teacher) {
-            return res.status(400).json({ error: 'Email or password is not correct' });
-        }
-
-        const isValid = await bcrypt.compare(password, Teacher.password);
-
-        if (!isValid) {
-            return res.status(400).json({ error: 'Email or password is not correct' });
-        }
-        const token = JWT.sign({ email, msg: 'I am Admin Level 0' }, process.env.AUTH_SECRET, {
-            expiresIn: '2 days'
-        })
-        res
-            .cookie('teacher', token, { httpOnly: true, maxAge: 2 * 24 * 60 * 60 * 1000 })
-            .redirect(200, '/index')
+    if (!Teacher) {
+      return res
+        .status(400)
+        .json({ error: "Email or password is not correct" });
     }
-    catch (error) {
-        next(error);
+
+    const isValid = await bcrypt.compare(password, Teacher.password);
+
+    if (!isValid) {
+      return res
+        .status(400)
+        .json({ error: "Email or password is not correct" });
     }
-})
+    const token = JWT.sign(
+      { email, msg: "I am Admin Level 0" },
+      process.env.AUTH_SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    );
+    res
+      .cookie("teacher", token, {
+        httpOnly: true,
+        maxAge: 2 * 24 * 60 * 60 * 1000,
+      })
+      .redirect(200, "/index");
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-router.post('/register', [
+router.post(
+  "/register",
+  [
     check("username", "Please enter the username for the user").notEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Please enter a password which is more than five characters').isLength({
-        min: 6
-    })
-], async (req, res, next) => {
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password which is more than five characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res, next) => {
     const { username, password, email } = req.body;
     const errors = validatorResult(req);
     try {
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const user = await teacher.find({ email });
+      console.log(user);
+      if (user && user.length !== 0) {
+        return res
+          .status(401)
+          .render("teacher/login", { error: "User is already registered !" });
+      }
+
+      const hashedPassword = await bcrpyt.hash(password, 12);
+
+      const newUser = new teacher({
+        name: username,
+        password: hashedPassword,
+        email,
+        isAdmin: 0,
+      });
+
+      await newUser.save();
+
+      const token = JWT.sign(
+        { email, msg: "I am admin level 0" },
+        process.env.AUTH_SECRET,
+        {
+          expiresIn: "2 days",
         }
-        const user = await teacher.find({ email });
-        console.log(user)
-        if (user && user.length !== 0) {
-            return res.status(401).render('teacher/login', { error: 'User is already registered !' })
-        }
+      );
 
-        const hashedPassword = await bcrpyt.hash(password, 12);
-
-        const newUser = new teacher({ name: username, password: hashedPassword, email, isAdmin: 0 })
-
-        await newUser.save();
-
-        const token = JWT.sign({ email, msg: 'I am admin level 0' }, process.env.AUTH_SECRET, {
-            expiresIn: '2 days'
+      res
+        .cookie("teacherCookie", token, {
+          httpOnly: true,
+          maxAge: 2 * 24 * 60 * 60 * 1000,
         })
-
-        res.cookie('teacherCookie', token, {
-            httpOnly: true,
-            maxAge: 2 * 24 * 60 * 60 * 1000
-        }).redirect('/teacher')
+        .redirect("/teacher");
     } catch (error) {
-        next(error);
+      next(error);
     }
-})
+  }
+);
 
-router.get('/logout', (req, res) => {
-    const token = req.cookies('teacherToken');
-    res.clearCookie('teacherCookie', { httpOnly: true, maxAge: 2 * 24 * 60 * 60 * 1000 })
-    res.redirect('/index')
-})
+router.get("/logout", (req, res) => {
+  const token = req.cookies("teacherToken");
+  res.clearCookie("teacherCookie", {
+    httpOnly: true,
+    maxAge: 2 * 24 * 60 * 60 * 1000,
+  });
+  res.redirect("/index");
+});
 
-router.get('/email', (req, res) => {
-    res.render('teacher/email');
-})
-router.get('/email/email_content', (req,res)=>{
-    res.render('teacher/email_content')
-})
+router.get("/email", (req, res) => {
+  res.render("teacher/email");
+});
+router.get("/email/email_content", (req, res) => {
+  res.render("teacher/email_content");
+});
 
-router.post('/email/create', checkAdmin, [
-    check('subject', 'Please include a subject in the email').isLength({
-        min: 1
+router.post(
+  "/email/create",
+  checkAdmin,
+  [
+    check("subject", "Please include a subject in the email").isLength({
+      min: 1,
     }),
-    check('message', ' Please include a message in the email').isLength({
-        min: 1
-    })
-], async (req, res, next) => {
+    check("message", " Please include a message in the email").isLength({
+      min: 1,
+    }),
+  ],
+  async (req, res, next) => {
     const { subject, message } = req.body;
-    const error = validatorResult(req)
+    const error = validatorResult(req);
 
     if (!error) {
-        return res.status(400).json({
-            error: error.array()
-        })
+      return res.status(400).json({
+        error: error.array(),
+      });
     }
 
     const email = new Email({
-        from: 'teacher', subject, message
-    })
+      from: "teacher",
+      subject,
+      message,
+    });
     email.save();
-    res.redirect('/index')
-})
+    res.redirect("/index");
+  }
+);
 
-router.get('/transfer/guidelines', (req, res) => {
-    res.render('teacher/transfer/transfer')
-})
+router.get("/transfer/guidelines", (req, res) => {
+  res.render("teacher/transfer/transfer");
+});
 
-router.get('/transfer/form', (req, res) => {
-    res.render('teacher/transfer/transfer-form')
-})
-router.post('/transfer/form/send', [
-    check('mobile-no', 'Please enter a valid phone number').isLength({ min : 10 }).isMobilePhone(),
-    check('email', 'Please enter a valid email').isEmail()
-], async (req,res,next)=>{
-    const { email, mobile_number, state, school_type, dob, present_school, subject, marital_status } = req.body;
-    const error = validationResult(req)
+router.get("/transfer/form", (req, res) => {
+  res.render("teacher/transfer/transfer-form");
+});
+router.post(
+  "/transfer/form/send",
+  [
+    check("mobile-no", "Please enter a valid phone number")
+      .isLength({ min: 10 })
+      .isMobilePhone(),
+    check("email", "Please enter a valid email").isEmail(),
+  ],
+  async (req, res, next) => {
+    const {
+      email,
+      mobile_number,
+      state,
+      school_type,
+      dob,
+      present_school,
+      subject,
+      marital_status,
+      date_of_joining,
+      city,
+      gender,
+      present_district,
+      current_designation,
+    } = req.body;
+    const error = validationResult(req);
     if (!error) {
-        return res.status(400).json({
-             error : error.array()
-         })
+      return res.status(400).json({
+        error: error.array(),
+      });
     }
     const transfer = new Transfer({
-        email , mobile_number, state, school_type, dob, present_school, subject, marital_status
-    })
+      email,
+      mobile_number,
+      state,
+      school_type,
+      dob,
+      present_school,
+      subject,
+      marital_status,
+      date_of_joining,
+      city,
+      gender,
+      present_district,
+      current_designation,
+    });
     transfer.save();
-    console.log(transfer)
-    res.redirect('/transfer/transfer-choices')
-    
-})
+    transferDB.push({
+      email,
+      mobile_number,
+      state,
+      school_type,
+      dob,
+      present_school,
+      subject,
+      marital_status,
+      date_of_joining,
+      city,
+      gender,
+      present_district,
+      current_designation,
+    });
+    res.redirect("/client/transfer/transfer-choices");
+  }
+);
 
-router.get('/transfer/transfer-choices', (req, res) => {
-    res.render('teacher/transfer/transfer-choices')
-})
-router.get('/transfer/preference-choices', (req, res) => {
-    res.render('teacher/transfer/transfer_preference_choices')
-})
-router.get('/transfer/ack', (req, res) => {
-    res.render('teacher/transfer/transfer_ack')
-})
-router.get('/transfer/finished', (req, res) => {
-    res.render('teacher/transfer/finished')
-})
+router.get("/transfer/transfer-choices", (req, res) => {
+  res.render("teacher/transfer/transfer-choices");
+});
+router.get("/transfer/preference-choices", (req, res) => {
+  res.render("teacher/transfer/transfer_preference_choices");
+});
+router.get("/transfer/ack", (req, res) => {
+  res.render("teacher/transfer/transfer_ack");
+});
+router.get("/transfer/finished", (req, res) => {
+  res.render("teacher/transfer/finished");
+});
 
-router.get('/study', (req, res) => {
-    res.render('teacher/study/study')
-})
+router.get("/study", (req, res) => {
+  res.render("teacher/study/study");
+});
 
-router.get('/todo', (req, res) => {
-    res.render('to_do')
-})
-router.get('/calendar', (req,res)=>{
-    res.render('teacher/calendar')
-})
-router.get('/view-portfolio',(req,res)=>{
-    res.render('teacher/portfolio')
-})
-router.get('/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const Teacher = await teacher.find({ _id: id });
-    if (!teacher) {
-        return res.status(400).redirect('/', { error: 'Teacher does not exist or is not registered.' })
-    }
-    res.json(teacher);
-})
+router.get("/todo", (req, res) => {
+  res.render("to_do");
+});
+router.get("/calendar", (req, res) => {
+  res.render("teacher/calendar");
+});
+router.get("/view-portfolio", (req, res) => {
+  res.render("teacher/portfolio");
+});
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const Teacher = await teacher.find({ _id: id });
+  if (!teacher) {
+    return res
+      .status(400)
+      .redirect("/", { error: "Teacher does not exist or is not registered." });
+  }
+  res.json(teacher);
+});
 
-router.get('/dailyUpdates', async (req, res, next) => {
-    try {
-        const updates = await DailyUpdates.find({});
-        res.json({ updates });
-    }
-    catch (error) {
-        next(error)
-    }
-})
+router.get("/dailyUpdates", async (req, res, next) => {
+  try {
+    const updates = await DailyUpdates.find({});
+    res.json({ updates });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
