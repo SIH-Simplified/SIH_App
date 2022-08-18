25c43f8df7262987ae2e62226d4c79f9fbecb959
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-18 14:02:02 +0530

    Added basic rendering routes for pages for superAdmin

--- my_app/routes/superAdmin.js
+++ my_app/routes/superAdmin.js
@@ -1,13 +1,32 @@
 const express = require("express");
-
+const Teacher = require("../models/teacher");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("superAdmin/index");
+router.get("/", async (req, res, next) => {
+    try {
+        const countOfTeachers = await Teacher.find({}).count();
+        res.render("superAdmin/index", { countOfTeachers });
+
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.get("/pushUpdates", (req, res) => {
     res.render("superAdmin/pushUpdates");
 })
+
+router.post("/pushUpdates/create", async (req, res, next) => {
+    const { districtName, update } = req.body;
+})
+
+router.get("/scheduleMettings", (req, res) => {
+    res.render("superAdmin/scheduleMettings");
+})
+
+router.get("/teacherTransfer", (req, res) => {
+    res.render("superAdmin/teacherTransfer");
+})
+
 
 module.exports = router;
