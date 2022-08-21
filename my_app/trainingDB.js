cdf5f128632b92c91573361c6a3d8861a486447a
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-21 13:00:24 +0530

    Added some dummy datain training DB

--- my_app/trainingDB.js
+++ my_app/trainingDB.js
@@ -6,5 +6,26 @@ module.exports = [
         dateTo: `${d.getDate() % 30 + 5}-${d.getMonth() % 12 + 1}-${d.getFullYear()}`,
         location: "Akiba , Japan",
         training_pdf: `https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`
+    },
+    {
+        title: "Techical training",
+        dateFrom: `${d.getDate()}-${d.getMonth()}-${d.getFullYear()}`,
+        dateTo: `${d.getDate() % 30 + 5}-${d.getMonth() % 12 + 1}-${d.getFullYear()}`,
+        location: "Akiba , Japan",
+        training_pdf: `https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`
+    },
+    {
+        title: "Techical training",
+        dateFrom: `${d.getDate()}-${d.getMonth()}-${d.getFullYear()}`,
+        dateTo: `${d.getDate() % 30 + 5}-${d.getMonth() % 12 + 1}-${d.getFullYear()}`,
+        location: "Akiba , Japan",
+        training_pdf: `https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`
+    },
+    {
+        title: "Techical training",
+        dateFrom: `${d.getDate()}-${d.getMonth()}-${d.getFullYear()}`,
+        dateTo: `${d.getDate() % 30 + 5}-${d.getMonth() % 12 + 1}-${d.getFullYear()}`,
+        location: "Akiba , Japan",
+        training_pdf: `https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf`
     }
 ]
